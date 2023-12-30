16ca8dcf610cce0eb62895d19f1dfdabce37e7c5
Diênifer <dienifersiqueira.dev@gmail.com>
2023-12-30 17:07:39 -0300

    Importa fontes Raleway e Lato do next/font/google.

---
+++ app/components/fonts.ts
@@ -0,0 +1,4 @@
+import { Raleway, Lato } from 'next/font/google';
+
+export const ralewayFont = Raleway({ subsets: ['latin'] });
+export const latoFont = Lato({ weight: '400', subsets: ['latin'] });
